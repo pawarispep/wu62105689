fcace50366dc7d09bed0884968ab02bf370ece58
pawarispep <71056642+pawarispep@users.noreply.github.com>
2021-08-13 14:13:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
-<h1>Pawaris Kongjan </h1>
-<iframe src="https://thingspeak.com/channels/1458413/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">
+<h1>Pawaris Kongjan 62105689 </h1>
+<iframe src="https://thingspeak.com/channels/1458413/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></ifram>
