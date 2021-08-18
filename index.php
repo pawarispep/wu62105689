6cef478e78b418071b27f950f9d285bc62bfa3c8
pawarispep <71056642+pawarispep@users.noreply.github.com>
2021-08-18 20:22:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <h2>Humidity</h2>
 <iframe src="https://thingspeak.com/channels/1458413/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" 
         style="border:none;" height="300px" width="500px"></iframe>
-
+<?php
         $api_url='https://api.thingspeak.com/channels/1458413/feeds.json?results=2';
         $json_data=file_get_content($api_url);
         $response_data=json_decode($json_data);
@@ -19,7 +19,7 @@
      <h3>Humidity<?php
         echo "Humidity: ".$humidity;
         ?></h3>
-<?php
+
         
         
         
