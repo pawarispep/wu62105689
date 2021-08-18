1316458caef4c480343051db9a2585f80c9a46c4
pawarispep <71056642+pawarispep@users.noreply.github.com>
2021-08-18 17:07:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <h1>Pawaris Kongjan 62105689 </h1>
-<h3>Humidity<h3>
+<h2>Humidity<h2>
 <iframe src="https://thingspeak.com/channels/1458413/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" 
+        style="border:none;" height="300px" width="500px"></iframe>
+<iframe src= "https://thingspeak.com/channels/1458413/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+        style="border:none;" height="300px" width="500px"></iframe>
+<iframe src= "https://thingspeak.com/channels/1458413/maps/channel_show" style="border:none; height="300px" width="500px"></iframe>
+<iframe src ="https://thingspeak.com/channels/1458413/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
         style="border:none;" height="300px" width="500px"></iframe>
