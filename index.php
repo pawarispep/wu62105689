285651dc4f15d0b71fa256d5bbb7f771cf3fd5ae
pawarispep <71056642+pawarispep@users.noreply.github.com>
2021-08-18 19:54:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,17 @@
 <h2>Humidity</h2>
 <iframe src="https://thingspeak.com/channels/1458413/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" 
         style="border:none;" height="300px" width="500px"></iframe>
+
+        
+        
+        
+<h2>Temperature</h2>
+<iframe src= "https://thingspeak.com/channels/1458413/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+        style="border:none;" height="300px" width="500px"></iframe>
+<h2>Map</h2>
+<iframe src= "https://thingspeak.com/channels/1458413/maps/channel_show" style="border:none;" height="500px" width="500px"></iframe>  
+</body>
+  
 <?php
         $api_url="https://api.thingspeak.com/channels/1458413/feeds.json?results=2";
         $json_data=file_get_content($api_url);
@@ -13,13 +24,5 @@
         echo"Humidity: ".$humidity[1];
         echo"Temperature: ".$temperature[1];
 ?>
-<h2>Temperature</h2>
-<iframe src= "https://thingspeak.com/channels/1458413/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
-        style="border:none;" height="300px" width="500px"></iframe>
-<h2>Map</h2>
-<iframe src= "https://thingspeak.com/channels/1458413/maps/channel_show" style="border:none;" height="500px" width="500px"></iframe>  
-</body>
-  
-
         
 
