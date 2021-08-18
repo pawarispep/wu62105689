dbbaa45550ae094f255d356b6eb2e5d489a2609b
pawarispep <71056642+pawarispep@users.noreply.github.com>
2021-08-18 17:26:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
 <h2>Temperature</h2>
 <iframe src= "https://thingspeak.com/channels/1458413/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
         style="border:none;" height="300px" width="500px"></iframe>
+<h2>Map</h2>
 <iframe src= "https://thingspeak.com/channels/1458413/maps/channel_show" style="border:none; height="300px" width="500px"></iframe>
-<iframe src ="https://thingspeak.com/channels/1458413/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
-        style="border:none;" height="300px" width="500px"></iframe>
+
