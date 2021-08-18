4654e051bfc814c13acdcebde4e0ce0c4f00d480
pawarispep <71056642+pawarispep@users.noreply.github.com>
2021-08-18 20:32:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,21 +4,32 @@
 <iframe src="https://thingspeak.com/channels/1458413/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" 
         style="border:none;" height="300px" width="500px"></iframe>
 <?php
-        $api_url='https://api.thingspeak.com/channels/1458413/feeds.json?results=2';
+        $url = "https://api.thingspeak.com/channels/1458413/feeds.json?results=2";
+$xml = simplexml_load_file($url);
+$channel_name = (string) $xml->name;
+print_r($channel_name);
+$field1 = $xml->xpath('//feed/field1'); //gets all <field1/> whose parent is <feed/>
+print_r($field1);
+$field2 = $xml->xpath('//feed/field2'); //gets all <field2/> whose parent is <feed/>
+print_r($field2);
+                
+                
+                
+                
+                
+                
+                
+                
+        /*$api_url='https://api.thingspeak.com/channels/1458413/feeds.json?results=2';
         $json_data=file_get_content($api_url);
         $response_data=json_decode($json_data);
         
         $humidity=$response_data->feeds[1]->field1;
         $temperature=$response_data->feeds[1]->field2;
-        
-        
-        
-       /* echo "Humidity: ".$humidity;
+       
+        echo "Humidity: ".$humidity;
         echo "Temperature: ".$temperature;*/
 ?>
-     <h3>Humidity<?php
-        echo "Humidity: ".$humidity;
-        ?></h3>
 
         
         
