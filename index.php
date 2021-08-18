5491320956b31ac3dd13ed6fb99aeccc5b063dbf
pawarispep <71056642+pawarispep@users.noreply.github.com>
2021-08-18 20:38:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,78 @@
-<body>
-<h1>Pawaris Kongjan 62105689 </h1>
-<h2>Humidity</h2>
-<iframe src="https://thingspeak.com/channels/1458413/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" 
-        style="border:none;" height="300px" width="500px"></iframe>
-<?php
-        $url = "https://api.thingspeak.com/channels/1458413/feeds.json?results=2";
-$xml = simplexml_load_file($url);
-$channel_name = (string) $xml->name;
-print_r($channel_name);
-$field1 = $xml->xpath('//feed/field1'); //gets all <field1/> whose parent is <feed/>
-print_r($field1);
-$field2 = $xml->xpath('//feed/field2'); //gets all <field2/> whose parent is <feed/>
-print_r($field2);
-                
-        /*$api_url='https://api.thingspeak.com/channels/1458413/feeds.json?results=2';
-        $json_data=file_get_content($api_url);
-        $response_data=json_decode($json_data);
-        
-        $humidity=$response_data->feeds[1]->field1;
-        $temperature=$response_data->feeds[1]->field2;
-       
-        echo "Humidity: ".$humidity;
-        echo "Temperature: ".$temperature;*/
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?php date_default_timezone_set('Asia/Bangkok');?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body onload="JavaScript:timedRefresh(15000);"><BR>
+    <?php
+    $arrContextOptions=array(
+        "ssl"=>array(
+            "verify_peer"=>false,
+            "verify_peer_name"=>false,
+        ),
+    );  
+    //ดึง json จาก API 
+$response = file_get_contents("https://api.thingspeak.com/channels/1458412/feeds.json?results=2", false, stream_context_create($arrContextOptions));
+$json = json_decode($response);
+//เลือกแค่ data
+$name = $json->channel->name;
+$hum = $json->feeds[1]->field1;
+$temp = $json->feeds[1]->field2;
 
-        
-        
-        
-<h2>Temperature</h2>
-<iframe src= "https://thingspeak.com/channels/1458413/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
-        style="border:none;" height="300px" width="500px"></iframe>
-<h2>Map</h2>
-<iframe src= "https://thingspeak.com/channels/1458413/maps/channel_show" style="border:none;" height="500px" width="500px"></iframe>  
-</body>
+$time = $json->feeds[1]->created_at;
+$newtime = date("d-m-Y H:i", strtotime($time)); 
+?>
+    
+<h1>Hamdee Naseng 6211273 </h1>
+<br>
+
+    
+<div class="container">
+  <div class="row">
+    <div class="col-sm-7">
+      <h3>Dashboard</h3>
+      <div class="container">
+        <div class="row">
+        <iframe  style="width: 500px; height: 300px " src="https://thingspeak.com/channels/1458412/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity&type=line"></iframe>
+        </div>
+    <br>
+        <div class="row">
+        <iframe style="width: 500px; height: 300px " src="https://thingspeak.com/channels/1458412/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature&type=line"></iframe>
+        </div>
+    <br>
+        <div class="row">
+        <iframe style="width: 500px; height: 300px " src="https://thingspeak.com/channels/1458412/maps/channel_show"></iframe>
+        </div>
+    <br>
+</div>
+    </div>
+    <div class="col-sm-5">
+      <h3>Data Latest</h3>
+      <div class="container">
+    <h3>Humidity : <?php echo $hum; ?> % <br>
+    Temperature: <?php echo $temp; ?> C <br>
+    Created at: <?php echo $newtime; ?>   <br>
+    </h3>
+</div>
+    </div>
+    
+  </div>
+</div>
+<script type="text/JavaScript">
+function timedRefresh(timeoutPeriod) {
+ setTimeout("location.reload(true);",timeoutPeriod);
+}
+
+</script>
+
+        </body>
+</html>
   
    
 
