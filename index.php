8b37bf1a29113dba6f1a5c25134e328259358cb6
pawarispep <71056642+pawarispep@users.noreply.github.com>
2021-08-18 21:23:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,21 @@
 <h2>Humidity</h2>
 <iframe src="https://thingspeak.com/channels/1458413/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" 
         style="border:none;" height="300px" width="500px"></iframe>
+<?php
+        $api=file_get_contents("https://api.thingspeak.com/channels/1458413/feeds.json?results=2");
+        $json=json_decode($api);
+        
+        $humidity=$json->feeds[1]->field1;
+        $temperature=$json->feeds[1]->field2;     
+?>        
+<div class="col-sm-5">
+        <h3>Data Lastest</h3>
+        <div class="container">
+        <h3>Humidity: <?php echo $humidity; ?><br>
+        Temperature: <?php echo $temperature; ?><br>
+                </h3>
+                </div>
+                </div>
 <h2>Temperature</h2>
 <iframe src="https://thingspeak.com/channels/1458413/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" 
         style="border:none;" height="300px" width="500px"></iframe>
@@ -10,13 +25,4 @@
 <iframe src="https://thingspeak.com/channels/1458413/maps/channel_show" 
         style="border:none;" height="300px" width="500px"></iframe>
 </body>
-<?php
-        $api=file_get_contents("https://api.thingspeak.com/channels/1458413/feeds.json?results=2");
-        $json=json_decode($api);
-        
-        $humidity=$json->feeds[1]->field1;
-        $temperature=$json->feeds[1]->field2;
 
-        echo $humidity;
-        echo $temperature;
-?>
