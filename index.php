597d8bc8424f798e144904fee8c878961316ab10
pawarispep <71056642+pawarispep@users.noreply.github.com>
2021-08-18 21:37:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <body>
 <h1>Pawaris Kongjan 62105689 </h1>
+<div class="container">
+        <div class="row">
+                <div class="col-sm-7">
+                        <div class="container">
+                                <div class="row">      
 <h2>Humidity</h2>
 <iframe src="https://thingspeak.com/channels/1458413/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" 
         style="border:none;" height="300px" width="500px"></iframe>
+                                 </div>
 <?php
         $api=file_get_contents("https://api.thingspeak.com/channels/1458413/feeds.json?results=2");
         $json=json_decode($api);
