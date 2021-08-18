54ebc4a8d6c403acce8747493053c65fec17d5d0
pawarispep <71056642+pawarispep@users.noreply.github.com>
2021-08-18 21:39:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <iframe src="https://thingspeak.com/channels/1458413/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" 
         style="border:none;" height="300px" width="500px"></iframe>
                                  </div>
+                                </div>
 <?php
         $api=file_get_contents("https://api.thingspeak.com/channels/1458413/feeds.json?results=2");
         $json=json_decode($api);
