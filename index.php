0d1000b60ae1ccc80ec7818d50f9311375572723
pawarispep <71056642+pawarispep@users.noreply.github.com>
2021-08-18 17:12:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <h2>Humidity<h2>
 <iframe src="https://thingspeak.com/channels/1458413/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" 
         style="border:none;" height="300px" width="500px"></iframe>
+<h2>Temperature</h2>
 <iframe src= "https://thingspeak.com/channels/1458413/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
         style="border:none;" height="300px" width="500px"></iframe>
 <iframe src= "https://thingspeak.com/channels/1458413/maps/channel_show" style="border:none; height="300px" width="500px"></iframe>
