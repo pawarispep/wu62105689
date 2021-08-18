909ea2a5e644107e1aaf58cfa9eaa7e0313d2751
pawarispep <71056642+pawarispep@users.noreply.github.com>
2021-08-18 19:55:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,17 @@
 <iframe src="https://thingspeak.com/channels/1458413/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" 
         style="border:none;" height="300px" width="500px"></iframe>
 
+<?php
+        $api_url="https://api.thingspeak.com/channels/1458413/feeds.json?results=2";
+        $json_data=file_get_content($api_url);
+        $response_data=json_decode($json_data);
+        $humidity=$response_data->field1;
+        $temperature=$response_data->field2;
+        echo "$response_data"; 
+        echo"Humidity: ".$humidity[1];
+        echo"Temperature: ".$temperature[1];
+?>
+     
         
         
         
@@ -14,15 +25,5 @@
 <iframe src= "https://thingspeak.com/channels/1458413/maps/channel_show" style="border:none;" height="500px" width="500px"></iframe>  
 </body>
   
-<?php
-        $api_url="https://api.thingspeak.com/channels/1458413/feeds.json?results=2";
-        $json_data=file_get_content($api_url);
-        $response_data=json_decode($json_data);
-        $humidity=$response_data->field1;
-        $temperature=$response_data->field2;
-        
-        echo"Humidity: ".$humidity[1];
-        echo"Temperature: ".$temperature[1];
-?>
-        
+   
 
