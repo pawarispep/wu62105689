2f430a2f054ef0348a6b6aa13228fe06acac432a
pawarispep <71056642+pawarispep@users.noreply.github.com>
2021-08-18 19:14:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <body>
 <h1>Pawaris Kongjan 62105689 </h1>
-<h2>Humidity<h2>
+<h2>Humidity</h2>
 <iframe src="https://thingspeak.com/channels/1458413/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" 
         style="border:none;" height="300px" width="500px"></iframe>
 <h2>Temperature</h2>
